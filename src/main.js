a0a7bb1d2abde42e41b189f800e5b7be895c171f
Bartosz Lewandowski <mebarlew@gmail.com>
2025-05-15 23:45:47 +0200

    start

--- src/main.js
+++ src/main.js
@@ -1,24 +1,17 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import "./style.css";
+import { Camera } from './components/Camera';
+const scene = new THREE.Scene();
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
 
-setupCounter(document.querySelector('#counter'))
+const directionalLight = new THREE.DirectionalLight();
+directionalLight.position.set(-100, -100, 200);
+scene.add(directionalLight);
+
+const camera = Camera();
+scene.add(camera);
+
+const renderer = Renderer();
+renderer.render(scene, camera);
